c25cc197bfc4e52699a2a869b5633c762b97b91d
Sos <sosmogendi@gmail.com>
2023-11-30 08:02:51 -0800

    Portfolio project MVP

---
+++ Portfolio/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+        <footer>
+          <p>&copy; {new Date().getFullYear()} Sospeter Nyabuto | Portfolio</p>
+        </footer>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
